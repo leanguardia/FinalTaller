0cf9280273890aa66bf71874f8e3e9eb1d1fafe5
Leonardo Velarde <leonardo.velarde.ldvc@gmail.com>
2015-05-11 13:13:47 -0400

    defauls goals

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  before_create :create_goals
+  after_create :create_goals
   def create_goals
     g1 = Goal.new :user_id => self.id, :goal_type_id => 1, :end_date => (Time.zone.now + 1.days), :target => 2000
     g1.save
