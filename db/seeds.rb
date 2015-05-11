730d3c190b42625a76bff1457b23b0d945760a98
Vinze Gonzales <vinze.g2@gmail.com>
2015-05-11 12:35:03 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,56 @@
 	test_user.active = true
 	test_user.save!
 
+	test_user = User.new
+	test_user.name = 'Usuario1'	
+	test_user.lastname = 'De Prueba'
+	test_user.email = 'user1@gmail.com'
+	test_user.password = 'usertest1'
+	test_user.sex = 'Male'
+	test_user.weight = '75.5'
+	test_user.height = '1.60'
+	test_user.birthdate = '10/10/1991'
+	test_user.rol = 'Client'
+	test_user.active = true
+	test_user.save!
+
+	test_user = User.new
+	test_user.name = 'Usuario2'	
+	test_user.lastname = 'De Prueba'
+	test_user.email = 'user2@gmail.com'
+	test_user.password = 'usertest2'
+	test_user.sex = 'Male'
+	test_user.weight = '76.5'
+	test_user.height = '1.65'
+	test_user.birthdate = '11/10/1991'
+	test_user.rol = 'Client'
+	test_user.active = true
+	test_user.save!
+
+	test_user = User.new
+	test_user.name = 'Usuario3'	
+	test_user.lastname = 'De Prueba'
+	test_user.email = 'user3@gmail.com'
+	test_user.password = 'usertest3'
+	test_user.sex = 'Male'
+	test_user.weight = '77.5'
+	test_user.height = '1.75'
+	test_user.birthdate = '11/10/1991'
+	test_user.rol = 'Client'
+	test_user.active = true
+	test_user.save!
+
+	test_band = Band.new
+	test_band.color = 'blue'
+    test_band.user_id = 1
+    test_band.Band_type_id = 1
+
+    test_band = Band.new
+	test_band.color = 'blue'
+    test_band.user_id = 2
+    test_band.Band_type_id = 1
+
+    test_band = Band.new
+	test_band.color = 'blue'
+    test_band.user_id = 3
+    test_band.Band_type_id = 1
