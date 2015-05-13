0bb4406a10eb63d297008fb8228133ea7667d10d
Leandro Guardia <lean.guardia93@gmail.com>
2015-05-13 01:00:04 -0400

    Bootstrap fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
-
+=begin
   validates :name, presence: {:message => "Usted debe ingresar su nombre completo"}
   validates :name, format: {:multiline => true, with: /^[a-zA-Z ]+$/, message: "Solo letras permitidas" }
   validates :name, length: {minimum: 10, maximum: 45, :message => "El nombre de usuario debe tener entre 10 y 45 caracteres"}  
-
-
+=end
   after_create :create_goals
   def create_goals
     g1 = Goal.new :user_id => self.id, :goal_type_id => 1, :end_date => (Time.zone.now + 1.days), :target => 2000
