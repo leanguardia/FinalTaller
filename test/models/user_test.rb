12d5d40bffbee639b284c15f01d4a07b0f530a87
MartinArispeRiveros <martinarisperiveros@gmail.com>
2015-05-22 00:38:14 -0400

    views users and unit tests user completed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test "given a user named user@gmail.com with a password of 123456, he should be authenticated with 'user@gmail.com' and '123456' " do
+    User.create( 
+    			:name => "Usuario",
+    			:lastname => "De Perueba",
+    			:email =>"user@gmail.com",
+                :password => "123456",
+    			:sex => "MAle",
+    			:weight => "70.5",
+    			:height => "1.70",          
+    			:birthdate => "10/10/1991"
+                :role => "Client",
+                :active => true)
+    assert User.authenticated?("homer", "123456")   
+  end
   # test "the truth" do
   #   assert true
   # end
