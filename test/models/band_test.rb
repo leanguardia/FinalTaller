fa41b505e7391b351622db19d0226b0e2449195e
AstronKing <vinze.g2@gmail.com>
2015-05-25 14:42:20 -0400

    One test created

--- test/models/band_test.rb
+++ test/models/band_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class BandTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  test "should get times option array" do
+    color = Array.new
+    color << ["green", 2]
+    color << ["red", 3]
+    color << ["blue", 4]
+    assert_equal(times, Band.get_color)
+  end
