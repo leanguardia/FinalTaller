26aa561ed4cbdac1529459088dd15e501bf821de
Leonardo Velarde <leonardo.velarde.ldvc@gmail.com>
2015-06-04 12:51:51 -0400

    fixed previous unsolved conflict

--- test/models/band_test.rb
+++ test/models/band_test.rb
@@ -1,16 +1,6 @@
 require 'test_helper'
 
 class BandTest < ActiveSupport::TestCase
-<<<<<<< HEAD
-  test "should get times option array" do
-    color = Array.new
-    color << ["green", 2]
-    color << ["red", 3]
-    color << ["blue", 4]
-    assert_equal(times, Band.get_color)
-	end
-end
-=======
   test 'should get a color array of 3 colors' do
     assert_equal(3, Band.get_color.count)
   end
@@ -20,4 +10,3 @@ end
   end
 
 end
->>>>>>> 31ca62af1cbc7566072c14f22a045005e470a715
