62fb46a5d6fd96d3e059e8865f55b67508e73cea
Leonardo Velarde <leonardo.velarde.ldvc@gmail.com>
2015-06-04 16:15:07 -0400

    adapting migration to postgres

--- db/migrate/20150509145104_create_goals.rb
+++ db/migrate/20150509145104_create_goals.rb
@@ -2,7 +2,7 @@ class CreateGoals < ActiveRecord::Migration
   def change
     create_table :goals do |t|
       t.datetime :start_date
-      t.datetime :end_date
+      t.integer :interval
       t.float :target
       t.float :reached
       t.references :goal_type, index: true
