0aa5d80f267ba4c44261574420608c3920700b9a
Leandro Guardia <lean.guardia93@gmail.com>
2015-06-08 01:01:05 -0400

    sleep model tests done

--- test/models/sleep_test.rb
+++ test/models/sleep_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class SleepTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @sleep = sleeps(:one)
+  end
+
+   test "should get the difference" do
+		difference = ((@sleep.awaken_at - @sleep.started_at) / 1.hour).round(2)	
+    assert_equal(difference, @sleep.getDifference)
+   end
 end
