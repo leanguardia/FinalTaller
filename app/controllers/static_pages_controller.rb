d497596dc6fbc136881a529e70932c16a2a82a3f
Leandro Guardia <lean.guardia93@gmail.com>
2015-06-24 04:40:23 -0400

    Interval dates to show chart, fixed heartrate chart

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,17 @@ class StaticPagesController < ApplicationController
 
   def dashboard
     if current_user
-      @band_data = User.find(current_user.id).band_data
+      @form = Date.today
+      @to = Date.today
+      if !params[:from]
+        @band_data = User.find(current_user.id).band_data
+      else
+        @from = Date.new(params[:from][:year].to_i,params[:from][:month].to_i,params[:from][:day].to_i)
+        @to = Date.new(params[:to][:year].to_i, params[:to][:month].to_i, params[:to][:day].to_i)
+        @band_data = BandDatum.where(:user_id=>current_user.id,:date_sent=> @from.beginning_of_day..@to.end_of_day)
+        #@band_data = User.find(current_user.id).band_data
+      end
+      @vars= [@band_data,@from,@to]
     else
       redirect_to '/'
     end
@@ -19,4 +29,5 @@ class StaticPagesController < ApplicationController
   	
   def about
   end
+
 end
