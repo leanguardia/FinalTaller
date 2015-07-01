49e237dd90bba1f7ac2393917d499984cf198970
Leonardo Velarde <leonardo.velarde.ldvc@gmail.com>
2015-07-01 00:25:46 -0400

    UI changes, generetor updated to new model

--- generator/generator.rb
+++ generator/generator.rb
@@ -8,28 +8,30 @@ user = gets.chomp
 puts 'Desired band id'
 band = gets.chomp
 puts 'Desired workout id (0 for none)'
-workout = gets.chomp
-if workout == 0
-  workout = nil
-end
+# workout = 0
+# workout = gets.chomp
+# if workout == 0
+#   workout = nil
+# end
 puts 'Start year'
 year = gets.chomp
 puts 'Start month'
 month = gets.chomp
 puts 'Start day'
 day = gets.chomp
-date = Time.new(year, month, day, 20, 40)
+date = Time.new(year, month, day, 00, 00)
 start_latitude = -17.378993
 longitude = -66.162296
 interval = 0.000205
 
-data = {'band_datum[user_id]' => user, 'band_datum[band_id]' => band,'band_datum[workout_id]' => workout, 'band_datum[date_sent]' => date, 'band_datum[steps_taken]' => 666, 'band_datum[calories_burnt]' => 666, 'band_datum[heart_rate_pminute]' => 666, 'band_datum[latitude]' => start_latitude, 'band_datum[longitude]' => longitude}
+data = {'band_datum[user_id]' => user, 'band_datum[band_id]' => band, 'band_datum[date_sent]' => date, 'band_datum[steps_taken]' => 666, 'band_datum[calories_burnt]' => 666, 'band_datum[heart_rate_pminute]' => 666, 'band_datum[latitude]' => start_latitude, 'band_datum[longitude]' => longitude}
 
 
-# uri = URI.parse('http://127.0.0.1:3000/band_data.json')
-uri = URI.parse('http://binaryband.herokuapp.com/band_data.json')
+#cambiar destino de los datos
+uri = URI.parse('http://127.0.0.1:3000/band_data.json')
+# uri = URI.parse('http://binaryband.herokuapp.com/band_data.json')
 
-# Full control
+
 http = Net::HTTP.new(uri.host, uri.port)
 
 Thread.new do
@@ -40,7 +42,6 @@ Thread.new do
 end
 
 loop do
-  date = date + 10*60
   sleep 0.01
   data['band_datum[date_sent]'] = date
   data['band_datum[steps_taken]'] = rand(100)
@@ -54,4 +55,5 @@ loop do
   request = Net::HTTP::Post.new(uri.request_uri)
   request.set_form_data(data)
   http.request(request)
+  date = date + 10*60
 end
